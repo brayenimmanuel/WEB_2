90d510138c19c622acde84fd7c4aac8be9730def
brayenimmanuel <afidhalim@gmail.com>
2025-03-13 10:45:14 +0700

    praktikum04

--- praktikum03/partials/modals/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'partials/modals/portofolio_1.php'; 
-include_once 'partials/modals/portofolio_2.php'; 
-include_once 'partials/modals/portofolio_3.php'; 
-include_once 'partials/modals/portofolio_4.php'; 
-include_once 'partials/modals/portofolio_5.php'; 
-include_once 'partials/modals/portofolio_6.php'; 
-?>
--- praktikum03/partials/section/index.php
+++
@@ -1,5 +0,0 @@
-<?php 
-include_once 'partials/section/portofolio.php';
-include_once 'partials/section/about.php';
-include_once 'partials/section/contact.php'; 
-?>
